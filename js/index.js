7b34623db33716a1fd948001fd614e37860d4fe9
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2020-11-30 19:54:54 -0500

    inital Commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,10 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Title
+document.title = 'Great Idea!'
+
+// Container
+let con = document.getElementsByTagName("a");
+con.setAttribute('href', siteContent["nav"]["nav-item-1"])
